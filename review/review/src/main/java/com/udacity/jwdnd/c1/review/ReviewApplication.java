737ae4139ee0cb4a955c02580af3547184fa9041
Val <gabrielvrcode@gmail.com>
2021-05-06 16:03:05 -0500

    completed lesson-4 thymeleaf and mvc review

--- review/review/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ review/review/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -2,7 +2,6 @@ package com.udacity.jwdnd.c1.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReviewApplication {
@@ -10,23 +9,4 @@ public class ReviewApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
-
-	@Bean
-	public String message() {
-		System.out.println("Message Bean Created");
-		return "Hello, Spring";
-	}
-
-	@Bean
-	public String uppercaseMessage(MessageService messageService) {
-		System.out.println("uppercaseMessage Bean Created");
-		return messageService.uppercase();
-	}
-
-	@Bean
-	public String lowercaseMessage(MessageService messageService) {
-		System.out.println("lowercaseMessage Bean Created");
-		return messageService.lowercase();
-	}
-
 }
---
+++ review/review/src/main/java/com/udacity/jwdnd/c1/review/service/MessageService.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.c1.review.service;
+
+import com.udacity.jwdnd.c1.review.model.ChatForm;
+import com.udacity.jwdnd.c1.review.model.ChatMessage;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MessageService {
+    private List<ChatMessage> chatMessages;
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Creating MessageService bean");
+        this.chatMessages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatForm chatForm) {
+        ChatMessage newMessage = new ChatMessage();
+        newMessage.setUsername(chatForm.getUsername());
+        switch (chatForm.getMessageType()) {
+            case "Say":
+                newMessage.setMessage(chatForm.getMessageText());
+                break;
+            case "Shout":
+                newMessage.setMessage(chatForm.getMessageText().toUpperCase());
+                break;
+            case "Whisper":
+                newMessage.setMessage(chatForm.getMessageText().toLowerCase());
+                break;
+        }
+        this.chatMessages.add(newMessage);
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
+    }
+}
